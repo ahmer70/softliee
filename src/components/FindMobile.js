7d016a3acbed0bcdd941ac61b30da5429d9e5bf4
Ahmer Arain <57433886+ahmerarain@users.noreply.github.com>
2022-08-07 15:47:38 +0500

    Configure React app for deployment to GitHub Pages

--- src/components/FindMobile.js
+++ src/components/FindMobile.js
@@ -8,7 +8,7 @@ const FindMobile = () => {
             <p className='right-border mb-2 ps-2 font26 font18' style={{ fontWeight: "bold" }}>Lets Find A Mobile Phone</p>
             <div className="mx-auto position-relative" >
                 <MobileRange />
-                <button type='button' className='find-btn ' style={{ backgroundColor: "#4958EF", fontSize: "24px", fontWeight: "bold", height: "68px", color: "white" }}>Find Mobile</button>
+                <button type='button' className='find-btn ' style={{ backgroundColor: "#4958EF", fontWeight: "bold", color: "white" }}>Find Mobile</button>
                 <div >
                     <p className='my-3'>RAM</p>
                     <div className='ram'>
